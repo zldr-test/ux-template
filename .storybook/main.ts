4eb4f12881b6ea35cab67e3cd0a15d3df5a2b626
Dorian Zlatan <zlatan.dorian272@gmail.com>
2026-06-14 03:10:10 +0300

    Initial commit

---
+++ .storybook/main.ts
@@ -0,0 +1,9 @@
+import type { StorybookConfig } from "@storybook/react-vite"
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: ["@storybook/addon-docs", "@storybook/addon-vitest"],
+  framework: "@storybook/react-vite",
+}
+
+export default config
